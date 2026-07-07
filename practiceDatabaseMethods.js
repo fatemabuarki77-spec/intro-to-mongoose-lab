961b532b7dbfca1da6317269da9d96dc3bf2b25b
fatemabuarki77-spec <Fatemabuarki77@gmail.com>
2026-07-07 18:02:57 +0300

    intro MDB lab fin

--- practiceDatabaseMethods.js
+++ practiceDatabaseMethods.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const env = require(`dotenv`).config();
 console.log(process.env.DATABASE_CONNECTION_STRING);
+
 const Recipe = require(`./models/Recipe`);
 
 const {
